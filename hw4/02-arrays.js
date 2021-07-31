6a7fb2b76f609b83928929d8cf98c2a315b6c12c
Alec Greenaway <aag3@pdx.edu>
2021-07-31 15:49:17 -0700

    exercise 1-3 done

--- hw4/02-arrays.js
+++ hw4/02-arrays.js
@@ -2,24 +2,45 @@ let result = [];
 
 const insertItem = (item) => {
   // insert item into result
+  result.push(item);
 
   return item;
 };
 
 const deleteItem = (item) => {
   // remove the first occurrence of item in result
+  result.splice(
+    result.findIndex((x) => x === item),
+    1
+  );
+  // result.forEach(function (arrayItem, index) {
+  //   if (item === arrayItem) {
+  //     result.splice(index, 1);
+  //     return item;
+  //   }
+  // });
 
   return item;
 };
 
 const lookupItem = (index) => {
   // return the item from result at index, deleting the obtained item
+  let removedItem = result[index];
+  result.splice(index, 1);
 
-  return index;
+  return removedItem;
 };
 
 const printItems = () => {
   // return a string of the concatenated item in result, separated by commas
+  let itemString = "";
+  result.forEach(function (arrayItem, index) {
+    itemString += arrayItem;
+    if (index !== result.length - 1) {
+      itemString += ",";
+    }
+  });
+  return itemString;
 };
 
 insertItem(1);
--- hw4/03-age.js
+++ hw4/03-age.js
@@ -1,5 +1,69 @@
+function isValidDate(d) {
+  return d instanceof Date && !isNaN(d);
+}
+
 const catculateAge = (birthday) => {
   // Given a date, return the age of the person with that birthday, in years, months, and days
+
+  let birthdayDate = new Date(birthday);
+  if (isValidDate(birthdayDate) === false) {
+    return "Error Invalid Input Provided";
+  }
+  let ageDif = Date.now() - birthdayDate.getTime();
+  let ageDate = new Date(ageDif);
+  let currentMonthDay = new Date(Date.now());
+
+  let years = ageDate.getFullYear() - 1970;
+  let months = ageDate.getMonth();
+  let days = null;
+  if (birthdayDate.getUTCDate() > currentMonthDay.getUTCDate()) {
+    days = Math.abs(birthdayDate.getUTCDate() - currentMonthDay.getUTCDate());
+  } else {
+    days = Math.abs(currentMonthDay.getUTCDate() - birthdayDate.getUTCDate());
+  }
+  let yearString = "";
+  let monthString = "";
+  let dayString = "";
+  let results = [];
+
+  if (years !== 0) {
+    if (years > 1) {
+      yearString = String(years + " Years");
+    } else {
+      yearString = String(years + " Year");
+    }
+    results.push(yearString);
+  }
+  if (months !== 0) {
+    if (months > 1) {
+      monthString = String(months + " Months");
+    } else {
+      monthString = String(months + " Month");
+    }
+    results.push(monthString);
+  }
+  if (days !== 0) {
+    if (days > 1) {
+      dayString = String(days + " Days");
+    } else {
+      dayString = String(days + " Day");
+    }
+    results.push(dayString);
+  }
+
+  let ageString = "Age: ";
+
+  results.forEach(function (item, index) {
+    ageString += item;
+    if (index !== results.length - 1) {
+      ageString += ", ";
+    }
+  });
+
+  // console.log(years + " Years");
+  // console.log(months + " Months");
+  // console.log(days + " Days");
+  return ageString;
 };
 
 // Date formats
@@ -8,6 +72,7 @@ const catculateAge = (birthday) => {
 // new Date(1995, 11, 17)
 // new Date(628021800000)
 
+birthday = "December 16, 1992";
 console.log(catculateAge(birthday));
 // Age: 25 years, 4 months, 3 days
 // Age: 25 years, 1 day
