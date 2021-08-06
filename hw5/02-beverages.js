41d68332a3c9a107c054eba5512e6fbb9fafe3dd
Alec Greenaway <aag3@pdx.edu>
2021-08-06 15:57:57 -0700

    added code to get drink names/pictures and create a flex grid to display on webpage.

--- hw5/02-beverages.js
+++ hw5/02-beverages.js
@@ -1,10 +1,56 @@
 const url =
-  'https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic';
+  "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic";
 
-let app = document.querySelector('#results');
+let drinkBox = document.getElementById("drinkContainer");
 
-const fetchData = (url) => {
-  // Add your code here
+function createNode(element) {
+  return document.createElement(element);
+}
+
+function append(parent, el) {
+  return parent.appendChild(el);
+}
+
+let createDrinkBox = (data) => {
+  // Create div
+  // Append following to div:
+  // -- fullName
+  // -- title
+  // -- Image
+  let div = createNode("div");
+  div.setAttribute("class", "charBox p-2 m-2 rounded");
+  let name = createNode("p");
+  let title = createNode("p");
+  let img = createNode("img");
+  img.src = data.strDrinkThumb;
+  img.style.height = "200px";
+  img.style.width = "200px";
+  name.innerHTML = data.strDrink;
+  // name.setAttribute("class", "charName");
+  // title.setAttribute("class", "charTitle");
+
+  // title.innerHTML = data.title;
+
+  append(div, img);
+  append(div, name);
+  append(div, title);
+  append(drinkBox, div);
 };
 
-fetchData(url);
+let app = document.querySelector("#results");
+
+async function fetchData(url) {
+  // Fetches an array of objects, each object is a drink
+  const data = fetch(url)
+    .then((response) => response.json())
+    .then((drinkArray) => drinkArray.drinks);
+
+  return data;
+}
+
+fetchData(url).then((drinks) => {
+  console.log(drinks);
+  drinks.forEach((element) => {
+    createDrinkBox(element);
+  });
+});
