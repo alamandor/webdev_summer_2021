644287037136d8914e067fffdb3491a7a4b96d2c
Alec Greenaway <aag3@pdx.edu>
2021-08-11 20:46:31 -0700

    finished 03, the jquery version of 02

--- hw5/03-beverages.js
+++ hw5/03-beverages.js
@@ -1,3 +1,49 @@
+const url =
+  "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic";
 $(document).ready(function () {
-  // Add your code here
+  const loading = $("#loading");
+
+  let createDrinkBox = (data) => {
+    // Create div
+
+    let name = $("<p>").text(data.strDrink);
+    let img = $("<img />", {
+      src: data.strDrinkThumb,
+    });
+    img.width(200);
+    img.height(200);
+
+    let div = $("<div>")
+      .addClass("figure p-2 m-2 rounded")
+      .append(img)
+      .append(name);
+    $("#drinkContainer").append(div);
+  };
+
+  async function fetchData(url) {
+    // Fetches an array of objects, each object is a drink
+    $.ajax({
+      type: "GET",
+      url: url,
+      dataType: "json",
+      success: (data) => {
+        console.log(data);
+        data.drinks.forEach((element) => {
+          createDrinkBox(element);
+        });
+      },
+      error: (error) => {
+        console.log(error);
+        let errorElement = $("<p>").text("An error occured. Please try again.");
+        $("#drinkContainer").append(errorElement);
+        // loading.parentElement.removeChild(loading);
+        loading.remove();
+      },
+    });
+  }
+
+  fetchData(url).then(() => {
+    // loading.parentElement.removeChild(loading);
+    loading.remove();
+  });
 });
